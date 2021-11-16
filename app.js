67b87bd1ddf8d03dc9eb7da9cc586d52fb128e54
rahul <cybngr@gmail.com>
2021-11-16 11:49:38 +0530

    srvr

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const http = require('http');
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
@@ -14,9 +15,26 @@ app.use("/api",categoryRouter);
 
 
 
-app.listen(process.env.PORT,()=>{
-	console.log(`Server connected...${process.env.port}`);
-})
+// app.listen(process.env.PORT,()=>{
+// 	console.log(`Server connected...${process.env.port}`);
+// })
+
+
+
+const hostname = '217.21.78.65';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('APi Working...\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
 mongoose.connect('mongodb://localhost:27017/phrmacy').then(c=>{
 	console.log("Database connected ...")
 });
